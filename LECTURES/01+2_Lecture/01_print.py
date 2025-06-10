cc6d4ac99492ace5ed7f70640516e9ce9474e88c
Fakrul Islam <fakrulislam0085@gmail.com>
2025-06-10 07:42:51 +0200

    Add files via upload
    Uploaded all the codes from Lecture slide 1 and Lecture slide 2

---
+++ LECTURES/01+2_Lecture/01_print.py
@@ -0,0 +1,13 @@
+print("Hello, Arif!")  #By default the print function starts a new line after its arguments are printed
+
+print("Hello, Fariha! My honey, I love You!")
+
+print("print my phone number: 3214578308")
+
+
+
+print("Goodbye!")
+print()     #A blank Line
+print("Hope to see you again")
+
+print('I am learning something new with manal')
