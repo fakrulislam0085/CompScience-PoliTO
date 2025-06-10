382e9f85f6158404dd602566554a0f93517b1f9f
Fakrul Islam <fakrulislam0085@gmail.com>
2025-06-10 12:35:38 +0200

    Update 02_Variables.py

--- LECTURES/01+2_Lecture/02_Variables.py
+++ LECTURES/01+2_Lecture/02_Variables.py
@@ -2,14 +2,14 @@
 age = 21
 
 #float 
-studentId = 3296.96
+studentId = 3296.96  # Variable name in camelCase
 height = 5.6
 
 #string
 name = 'Arif'
 
 #boolean
-is_googler = False
+is_googler = False  # Variable name in snake_case
 is_student = True
 
 print(name, "is", age, "years old. His Student id is", studentId,".He is a googler =", is_googler)
