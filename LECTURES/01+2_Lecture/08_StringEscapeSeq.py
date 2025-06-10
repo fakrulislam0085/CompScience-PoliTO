a181e3bacbd79fd4e820b8e0becb898abcd8d85c
Fakrul Islam <fakrulislam0085@gmail.com>
2025-06-10 12:47:43 +0200

    Update 08_StringEscapeSeq.py

--- LECTURES/01+2_Lecture/08_StringEscapeSeq.py
+++ LECTURES/01+2_Lecture/08_StringEscapeSeq.py
@@ -1,6 +1,7 @@
-#Printing double quote
+#Printing double and single quote 
 print(" He said \"Hello\" ")
 print(' He said "Arif" ')
+print(" He said 'I'")
 
 #printing slash
 print(" C: \\Temp\\Secret.txt ")
