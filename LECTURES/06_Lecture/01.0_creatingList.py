4b84057eb632e38310ad9d0b35c28332daf28c7a
Fakrul Islam <fakrulislam0085@gmail.com>
2025-06-16 17:27:00 +0200

    Add files via upload

---
+++ LECTURES/06_Lecture/01.0_creatingList.py
@@ -0,0 +1,27 @@
+#creating a integer list
+values = [1, 34, 34, 32, 76, 46, 12]
+
+#accessing list element
+print(values[0])
+
+#replacing list element
+values[0] = 10 
+print(values[0])
+
+
+#list of float values
+float_num = [12.3, 34.5, 6.7, 1.50]
+print(float_num)
+    
+
+#list of days name
+day_name = ['sat', 'sun', 'mon', 'wed', 'thur', 'fri']
+day_name[3] = 'Tue'
+print(day_name)
+
+
+#list of mix values
+mix_list = [1, 'abc', 12.32, 'h']
+l = len(mix_list)
+print("Length of mix_list:", l)
+print(mix_list)
---
+++ LECTURES/06_Lecture/01.2_listAliases.py
@@ -0,0 +1,17 @@
+'''When you copy a list variable into another, both variables refer to the same list,
+this is called list Aliases''' 
+scores = [10, 20, 30, 40, 50]
+values = scores 
+
+print(values)
+scores[2] = 300     #You can modify the list through either of the variables
+print(values[2])    # we modified scores[2]; but our values[2] will be also modified
+print(values)
+print(scores)
+
+two_from_last = scores[-2]      # In case of reversing, index starts from 1 
+print("The second value form last in the scores list is:", two_from_last)
+
+
+
+
