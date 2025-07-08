8f62b7e908329e4cbd98b4677f1e112ea6add855
Fakrul Islam <fakrulislam0085@gmail.com>
2025-07-08 16:18:57 +0200

    Add files via upload

---
+++ LABS/LabExercises12/ExamSimulation/hotelRecords.py
@@ -0,0 +1,74 @@
+def process(file1, file2, file3) : 
+    try : 
+        with open(file1, "r") as customersFile, open(file2, "r") as suspectsFile, open(file3, "w") as writeFile : 
+            # Read the suspects name 
+            suspectsList = suspectsFile.readlines() 
+            for Sname in suspectsList : 
+                Sname = Sname.strip()
+
+                # Reset the file pointer to the beginning of customersFile
+                customersFile.seek(0)
+
+                # Read the customers file 
+                customersList = customersFile.readlines()
+
+                # Find out the suspects in and out day
+                for line in customersList : 
+                    line = line.strip()
+                    fields = line.split(',') 
+                    
+                    name, number, checkIn, checkOut = fields 
+
+                    if Sname == name : 
+                        try: 
+                            SuspectsInD = int(checkIn)
+                            suspectsOutD = int(checkOut)
+                        except ValueError : 
+                            print("Something wrong with the value.")
+                            return 
+                        
+                # now compare with the file 
+                writeFile.write(f"** Contacts of the guest: {Sname}: **\n")
+                print(f"** Contacts of the guest: {Sname}: **")
+                contacts = []   # Collect contacts in a list
+                for line in customersList : 
+                    line = line.strip()
+                    fields = line.split(',') 
+                    
+                    name, number, checkIn, checkOut = fields 
+
+                    try : 
+                        checkIn = int(checkIn) 
+                        checkOut = int(checkOut)
+
+                    except ValueError : 
+                        print("Something wrong with the value.")
+                        return 
+                    
+                    if Sname != name :
+                        if SuspectsInD in range(checkIn, checkOut+1) or suspectsOutD in range(checkIn, checkOut+1):
+                            contacts.append((name, number))                      
+
+                if contacts :
+                    contacts.sort(key=lambda x : x[0])
+                    for contact in contacts : 
+                        print(f"\tContact with {contact[0]}, phone {contact[1]}")
+                        writeFile.write(f"\tContact with {contact[0]}, phone {contact[1]}\n")
+                else : 
+                    print(f"\tThe guest {Sname} had no contacts")
+                    writeFile.write(f"\tThe guest {Sname} had no contacts\n")
+
+
+    except FileNotFoundError : 
+        print(f"{file1/file2/file3}: Not found.")
+        
+
+def main() : 
+    file1 = "customers.txt" 
+    file2 = "suspects.txt" 
+    file3 = "outcome.txt" 
+
+    process(file1, file2, file3) 
+
+if __name__ == "__main__" : 
+    main() 
---
+++ LABS/LabExercises12/ExamSimulation/sol2.py
@@ -0,0 +1,84 @@
+FILENAME1 = "customers.txt"
+FILENAME2 = "suspects.txt" 
+
+suspectList = []
+customerDict = dict() 
+
+def findTheContacts() : 
+    for sName in suspectList : 
+        if sName in customerDict : 
+            sIn = customerDict[sName]['Check-in'] 
+            sOut = customerDict[sName]['Check-out'] 
+            try : 
+                sIn, sOut = int(sIn), int(sOut) 
+            except ValueError : 
+                print("Couldn't convert the values to int")
+            # print(f"{sName} : {sIn}, {sOut}")
+
+            print(f"** Contacts of the guest: {sName} ** ")
+            foundConnection = False
+            collectContacts = [] 
+
+            for cName in customerDict :     # cName is a dict inside the customersDict
+                cIn = customerDict[cName]['Check-in']    # accessing the value based on assigned key 
+                cOut = customerDict[cName]['Check-out']
+                #print(f"{cName} : {cIn}, {cOut}")
+
+                try : 
+                    cIn , cOut = int(cIn), int(cOut) 
+                except ValueError : 
+                    print("Couldn't convert the values to int")
+
+                if cName != sName :
+                    if cIn in range(sIn, sOut+1) or cOut in range(sIn, sOut): 
+                        foundConnection = True
+                        collectContacts.append((cName, customerDict[cName]['Phone']))
+                        # print(f"\tContact with {cName}, phone {customerDict[cName]['Phone']}")
+
+            if foundConnection == False : 
+                print(f"\tThe guest {sName} had no contacts")
+            else : 
+                sortedContacts = sorted(collectContacts, key=lambda x : x[1]) 
+                for n, p in sortedContacts : 
+                    print(f"\tContact with {n}, phone {p}")
+
+
+def main() : 
+    try : 
+        with open(FILENAME2, 'r') as suspectF : 
+            for name in suspectF : 
+                suspectList.append(name.strip()) 
+    except FileNotFoundError : 
+        print(f"{FILENAME2}: Is not found.") 
+    
+    # print(suspectList)
+
+    try : 
+        with open(FILENAME1, 'r') as customersF : 
+            for line in customersF : 
+                line = line.strip().split(',') 
+
+                if len(line) == 4 : 
+                    name, phone, inD, outD = line 
+                    customerDict[name] = {'Phone':phone, 'Check-in': inD, 'Check-out': outD}
+
+                    try : 
+                        inD, outD = int(inD) , int(outD) 
+
+                    except ValueError : 
+                        print("Couldn't conver the values to int.")
+                
+                else : 
+                    print("Couldn't translate the line.")
+                    continue 
+
+    except FileNotFoundError : 
+        print(f"{FILENAME1}: Is not found.") 
+    
+    # print(customerDict) 
+
+    findTheContacts()
+
+
+if __name__ == "__main__" : 
+    main() 
