3b004c95444a3c340656ad9a4cd8f617b43e97f1
Fakrul Islam <fakrulislam0085@gmail.com>
2025-07-11 23:38:51 +0200

    Add files via upload

---
+++ LABS/LabExercises11/11.1.1_CountingWords/11.1.2_MostFrequentWords/MostFreWord.py
@@ -0,0 +1,37 @@
+def mostFreqWords(fileName) : 
+    resultDic = dict()     
+
+    try: 
+        with open(fileName, "r") as inFile : 
+            words = inFile.read().strip()
+            words = words.split()
+
+            for singleWord in words : 
+                singleWord = singleWord.lower()
+
+                if singleWord in resultDic : 
+                    resultDic[singleWord] += 1 
+                else : 
+                    resultDic[singleWord] = 1 
+    except FileNotFoundError : 
+        print(f"{fileName}: Not found\n") 
+    
+    return resultDic 
+
+def main() : 
+    fileName = "input.txt" 
+    wordDict = mostFreqWords(fileName) 
+
+    # Sorted the dictionary based on it's value 
+    # wordDict = dict(sorted(wordDict.items(), key=lambda x: x[1], reverse=True))
+    wordDict = sorted(wordDict.items(), key=lambda x : x[1], reverse=True)  # now wordDict is a list of tuples(word, count)
+
+    # Get the first 5 key-value pairs 
+    topFiveWords = wordDict[:5]
+
+    # Display the result
+    for word, count in topFiveWords : 
+        print(f"{word:<15}{count}")
+
+if __name__ == "__main__" : 
+    main() 
