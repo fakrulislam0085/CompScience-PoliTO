03cc2b02cbf18b43694f05fdb5db8ce09d9bc4de
Fakrul Islam <fakrulislam0085@gmail.com>
2025-07-14 23:00:11 +0200

    Add files via upload

---
+++ LABS/LabExercises11/11.1.5_SparseVectors/SparseVectors.py
@@ -0,0 +1,24 @@
+def sparse_array_sum(a, b) : 
+    resultDict = dict(a) 
+
+    for key in b : 
+        if key in resultDict :
+            resultDict[key] += b[key]
+        else :
+            resultDict[key] = b[key]
+        
+    return resultDict
+
+def main() : 
+    # Two sparse dictionary
+    a = {5: 4, 9: 2, 10: 9}
+    b = {5: 4, 8: 4, 10: 4}
+
+    sum_of_a_b = sparse_array_sum(a, b) 
+
+    print(f"A is: {a}")
+    print(f"B is: {b}")
+    print(f"The sum of a + b as a dictionary: {sum_of_a_b}")
+
+if __name__ == "__main__" : 
+    main() 
