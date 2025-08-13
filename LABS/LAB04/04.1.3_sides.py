b0fa55c1585cd9b7f0f89cc0cbe46fcf817b96f7
Phoenix <fakrulislam0085@gmail.com>
2025-08-14 00:20:05 +0200

    Add exercise 3 to the LAB04 folder

---
+++ LABS/LAB04/04.1.3_sides.py
@@ -0,0 +1,33 @@
+def main() : 
+    n = int(input("Enter the side length(n): ")) 
+
+    # Printing the full square
+    for i in range(n) : 
+        for j in range(n) : 
+            print("*", end="")
+        print() 
+    
+
+    # Printing the Rhombus
+    print()
+    asterisks = 1 
+    spaces = n-1 
+    line = 2*n - 1 
+    for i in range(line) : 
+        if i<n :    # Up shape 
+            print(spaces*' ' + asterisks*'*')
+
+            if i != n-1 :
+                spaces -= 1
+                asterisks += 2 
+
+        else :  # Down shape 
+            asterisks -= 2 
+            spaces += 1 
+
+            print(spaces*' ' + asterisks*'*')
+            
+
+
+if __name__ == "__main__" : 
+    main() 
