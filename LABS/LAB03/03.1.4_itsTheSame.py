6814562fe9118ab34158d689ec447a25ab394aa9
Phoenix <fakrulislam0085@gmail.com>
2025-08-29 08:01:58 +0200

    Pushed solution of exercise 3.1.4 of Lab03

---
+++ LABS/LAB03/03.1.4_itsTheSame.py
@@ -0,0 +1,20 @@
+def main() : 
+    x = 3.0 
+    s == 'seven point five' 
+    if x = 3.0 : 
+        s == 'three dot zero' 
+    print(s) 
+    
+
+    # This code contains multiple syntax errors. If you want to run this code, don't forget to comment the wrong code. 
+    # Now let's fix all of the errors! 
+    x = 3.0 
+    s = 'seven point five'      # We are assigning a value; we can't use relational operator here. 
+    if x == 3.0 :       # Relational operator goes with condition. 
+        s = 'three point zero' 
+    print(s)
+    
+if __name__ == "__main__" : 
+    main() 
+
+
