bd9f5020f0f9c07b36a0acc134326838d104613c
Phoenix <fakrulislam0085@gmail.com>
2025-09-11 21:06:11 +0200

    Update: Solution of Exercise 2.4

---
+++ LABS/LAB05/05.2.4_transformer.py
@@ -0,0 +1,24 @@
+# We are not talking about Transformer Movie guys!
+
+R0 = 20     # ohm
+VS = 40     # Volt
+RS = 8      # ohm
+
+def main() : 
+    max_power = -1 
+    max_turns_ratio = -1
+
+    n = 0.01    # turns ratio
+    while n <= 2.0 :
+        Ps = RS * (n * VS / (n**2 * R0 + RS))**2
+
+        if Ps > max_power : 
+            max_power = Ps 
+            max_turns_ratio = n 
+
+        n += 0.01
+
+    print(f"The maximum power is {max_power:.2f} with turns ratio of {max_turns_ratio:.2f}")
+
+if __name__ == "__main__" : 
+    main() 
