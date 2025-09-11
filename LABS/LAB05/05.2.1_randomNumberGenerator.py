05d5c9ee5c44bc458d07b9ec43efe811073dc8b7
Phoenix <fakrulislam0085@gmail.com>
2025-09-11 21:05:17 +0200

    Update: Solution of Exercise 2.1

---
+++ LABS/LAB05/05.2.1_randomNumberGenerator.py
@@ -0,0 +1,19 @@
+A = 32_310_901    # Underscores don’t change the value; they just make big numbers easier to read.
+B = 1_729
+M = 2e24
+
+def main() : 
+    r_old = int(input("Enter the initial value(the seed): "))
+
+    for _ in range(100) : 
+        r_new = (A * r_old + B) % M 
+        r_old = r_new 
+
+        print(f"The new numbers is {r_new:.3f}")
+
+if __name__ == "__main__" : 
+    main() 
+    
+
+# Python supports underscores in integer and float literals (e.g. 1_000_000, 3.14_15) since Python 3.6.
+
